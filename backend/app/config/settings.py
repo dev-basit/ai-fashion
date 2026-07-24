8c18890dfd1f1a09a390206854455bf46c7680eb
Abdul Basit <dev.basit2020@gmail.com>
2026-07-24 19:39:44 +0500

    using proper types for secrets

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,18 +1,20 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from pydantic import SecretStr
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = ""
-    SECRET_KEY: str = ""
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 10
-    REFRESH_TOKEN_EXPIRE_MINUTES: int = 300
-    OPENAI_API_KEY: str = ""
-
+    OPENAI_API_KEY: SecretStr
     IS_LLM_LIMIT: bool = True
     LLM_REQUESTS_PER_DAY: int = 100
+
+    DATABASE_URL: str
+
+    JWT_SECRET_KEY: SecretStr
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 10
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 300
+
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
-settings = Settings()
+settings = Settings() # type: ignore
