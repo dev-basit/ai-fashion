8d77e7f734024e0bd01ab50546619fefc2161d1f
Abdul Basit <dev.basit2020@gmail.com>
2026-07-24 19:27:39 +0500

    added fallback values

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -2,12 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    SECRET_KEY: str
+    DATABASE_URL: str = ""
+    SECRET_KEY: str = ""
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 10
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 300
-    OPENAI_API_KEY: str
+    OPENAI_API_KEY: str = ""
 
     IS_LLM_LIMIT: bool = True
     LLM_REQUESTS_PER_DAY: int = 100
