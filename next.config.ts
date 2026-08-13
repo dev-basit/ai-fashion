afd1119d9f06e6db7655ec413fe0edf7fe853a30
Abdul Basit <dev.basit2020@gmail.com>
2026-08-13 11:54:04 +0500

    added config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "http", hostname: "127.0.0.1", port: "54321", pathname: "/storage/v1/object/**" },
+      { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
 };
